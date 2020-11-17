1ed1c66fb732a186d3ba9ecbb9b0aae1399bbde0
Bohdan Protsiuk <bohdan.protsiuk@gmail.com>
2020-11-17 09:17:29 +0200

    v3 dev poc

---
+++ src/v4/types.ts
@@ -0,0 +1,34 @@
+export type FieldErrorType = string;
+export interface FieldValidatorInterface {
+  id: string;
+  callback: (value: any) => string | boolean;
+}
+export interface FieldSetterInterface {
+  id: string;
+}
+export interface FieldGetterInterface {
+  id: string;
+  callback: (value: any) => void;
+}
+
+export interface FieldInterface {
+  id: string;
+  isValidating: boolean;
+  isDisabled: boolean;
+  isTouched: boolean;
+  isValid: boolean;
+  value: any;
+  setters: FieldSetterInterface[];
+  getters: FieldGetterInterface[];
+  validators: FieldValidatorInterface[];
+  errors: FieldErrorType[];
+}
+
+export interface FormStateInterface {
+  defaultValidators: {};
+  defaultValues: {};
+  fields: FieldInterface[];
+  isValidating: boolean;
+  isTouched: boolean;
+  isValid: boolean;
+}
