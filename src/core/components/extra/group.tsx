cc406db04854fe6e59922c9246eee27c38d8c9f6
mexxdz <mkostyrin@gmail.com>
2023-04-03 15:18:07 +0300

    fix zustand imports (#36)

--- src/core/components/extra/group.tsx
+++ src/core/components/extra/group.tsx
@@ -1,6 +1,6 @@
 import React, { CSSProperties, PropsWithoutRef, ReactNode } from 'react';
 import { useGroup } from '../../hooks';
-import create, { StoreApi } from 'zustand';
+import { create, StoreApi } from 'zustand';
 import createContext from 'zustand/context';
 import { FieldRefProp, GroupApiInterface, GroupsApiInterface } from '../../types';
 
